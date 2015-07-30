0f76f397c8cb9f22698e614f72649738ab0baabd
Kévin <Kvin@Kvin-PC>
2015-07-30 22:04:07 +0200

    Ajout des fichiers du framework bootstrap (js, css, icon) et des premiers fichiers src.

---
+++ src/cv.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+  <head>
+	<title>Mon site perso</title>
+	<!-- Bootstrap -->
+    <link href="../bootstrap/css/bootstrap.css" rel="stylesheet">
+	<script type="text/javascript" src="../bootstrap/js/bootstrap.js"></script>
+  </head>
+  <body>
+  	<?php include 'navBar.html';?>
+	<div class="row">
+	<div class="col-md-7 col-md-offset-1">
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<h4>A propos de moi !</h4>
+			</div>
+			<div class="panel-body">
+				<p>C'est au lycée que m'est apparue la passion pour l'informatique. L'influence croissant de ce domaine sur nos vies m'a toujours impréssionné. C'est pourquoi j'ai voulue faire dans études d'informatique pour pouvoir faire partie de cette aventure.</br><p>
+				<p>Je recherche actuellement un stage de 3 mois commencant le 18 Mai 2015 dans une soci�t� d'informatique.</p>
+				<p>Mon CV est telechargeable en version PDF <a href="cv-letupe.pdf">ici</a></p>
+			</div>
+		</div>
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<h4>Formations suivies :</h4>
+			</div>
+			<div class="panel-body">
+				<p>
+					<ul class="li-cv">
+						<li>2013/2016 : diplôme d’ingénieur, génie informatique et statistique, Polytech Lille : prise en main et maitrise d’outils statistiques, informatiques et managériales
+						</li>
+						<li>2011/2013 : DUT Informatique, l’IUT A de Lille : apprentissage de divers langages de programmations. Maitrise des systèmes de gestion de bases de données. Notion de réseaux
+						</li>
+						<li>juin 2011 : Baccalauréat scientifique au lycée Charles de Gaulle à Compiègne
+						</li>
+					</ul>
+				</p>
+			</div>
+		</div>
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<h4>Expériences Professionnelles :</h4>
+			</div>
+			<div class="panel-body">
+				<p>
+					<ul class="li-cv">
+						<li>Juillet/Aout 2012 - 2013 - 2014, magasinier à Conforama Compiègne : 
+							<ul>
+								<li>service des clients</li>
+								<li>réception et rangement des produits</li>
+								<li>réapprovisionnement du magasin</li>
+							</ul>
+						</li>
+						<li>Avril/Juin 2013 : stage de fin de DUT au Danemark : réalisation d’un projet de reconnaissance facial avec une équipe de recherche
+						</li>
+						<li>Décembre 2012 - Décembre 2013 : participation à la nuit de la programmation. Réalisation de projets informatiques en équipe, comme un mini site de E-commerce.
+						</li>
+						<li>2009/2011 : directeur de publication dans un journal lycéen : 
+							<ul>
+								<li>organisation de l’équipe journalistique</li>
+								<li>écriture de l’éditorial du journal</li>
+								<li>rédaction d’articles sur l’actualité internationale</li>
+							</ul>
+						</li>
+					</ul>
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<h4>Compétences informatiques :</h4>
+			</div>
+			<div class="panel-body">
+				<p>Langages connues :</br>
+					<span class="label label-info">Java</span>
+					<span class="label label-info" data-toggle="popover" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus." >PHP</span>
+					<span class="label label-info">HTML</span>
+					<span class="label label-info">CSS</span>
+					<span class="label label-info">C/C++</span>
+					<span class="label label-info">XML</span>
+					<span class="label label-info">SQL</span>
+					<span class="label label-info">Latex</span>
+				</p>
+				<p>Systeme d'exploitation connues :</br>
+					<span class="label label-info">Linux</span>
+					<span class="label label-info">Windows</span>
+					<span class="label label-info">AS400</span>
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<h4>Langues</h4>
+			</div>
+			<div class="panel-body">
+				<p>
+					<ul>
+						<li>Anglais : courant</li>
+						<li>Espagnol : intermédiaire</li>
+					</ul>
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<h4>Centres d'intêrets</h4>
+			</div>
+			<div class="panel-body">
+				<p>
+					<ul>
+						<li>Littérature épistolaire et théâtral</li>
+						<li>Musique rap et hip-hop</li>
+						<li>Culture asiatique</li>
+						<li>Datajournalisme</li>
+						<li>Actualités et informations</li>
+					</ul>
+				</p>
+			</div>
+		</div>
+	</div>
+	</body>
+</html>
---
+++ src/index.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>Mon site perso</title>
+		<link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+	</head>
+	<body>
+		<?php include 'navBar.html';?>
+	</body>
+</html>
