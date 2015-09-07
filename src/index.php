a4845c1d4e0483a091dab576d8d322ec96db99e0
Kévin <Kvin@Kvin-PC>
2015-09-07 15:31:55 +0200

    changement de l'index

--- src/index.php
+++ src/index.php
@@ -1,9 +1,20 @@
 <html>
 	<head>
-		<title>Mon site perso</title>
+		<title>Kévin Letupe</title>
 		<link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
 	</head>
 	<body>
-		<?php include 'navBar.html';?>
+		<!--<?php include 'navBar.html';?>-->
+		<nav class="navbar navbar-default navbar-fixed-top">
+			<div class="container-fluid">
+				<div class="navbar-header"><a class="navbar-brand">Le Savoir est une Arme !</a></div>
+				<!-- <h2 class="navbar-left"><a href="#">Le savoir est une arme</a></h2>  -->
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#">Projets</a></li>
+					<li><a href="#">CV</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+		</nav>
 	</body>
 </html>
