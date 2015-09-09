00c77d7baa2c3e35a46bfd23dabaedfc00a54bb6
Kévin <Kvin@192.168.1.36>
2015-09-09 23:26:46 +0200

    ajout de font-awesome modification de page acceuil ajout des photos

--- src/index.php
+++ src/index.php
@@ -1,18 +1,22 @@
+<!DOCTYPE html>
 <html>
 	<head>
-		<title>K�vin Letupe</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Kévin Letupe</title>
 		<link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
 		<link rel="stylesheet" href="../css/personalWebSite.css">
+		<link rel="stylesheet" href="../font-awesome/css/font-awesome.css">
+		
 	</head>
 	<body>
 		<!--<?php include 'navBar.html';?>-->
 		<nav class="navbar navbar-default navbar-fixed-top">
 			<div class="container-fluid">
-				<div class="navbar-header"><a class="navbar-brand">Le Savoir est une Arme !</a></div>
+				<div class="navbar-header"><a href="#" class="navbar-brand">Le Savoir est une Arme !</a></div>
 				
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href="#">Projets</a></li>
-					<li><a href="#">CV</a></li>
+					<li><a href="#projets">Projets</a></li>
+					<li><a href="#CV">CV</a></li>
 					<li><a href="#">Contact</a></li>
 				</ul>
 			</div>
@@ -21,15 +25,75 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-lg-12">
-						<img class="img-responsive" src="image3344.png" alt="">
+						<img class="img-responsive" src="../img/image3344.png" alt="">
 						<div class="intro-text">
-                        	<span class="title">K�vin Letupe</span>
+                        	<span class="title">Kévin Letupe</span>
                         	<hr class="star-light">
-                        	<span class="skills">Web Developer - Graphic Artist - User Experience Designer</span>
+                        	<span class="skills">Etudiant à Polytech'Lille - Java/Web développeur</span>
 						</div>
 					</div>
 				</div>
 			</div>
 		</header>
+		<section id="projets">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 text-center">
+						<h2>Projets réalisés</h2>
+						<hr class="star-primary">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-4 portfolio-item">
+						<a href="#portfolioItem1" class="portfolio-link" data-toggle="modal">
+							<div class="caption">
+                            	<div class="caption-content">
+                                	<i class="fa fa-search-plus fa-3x"></i>
+                            	</div>
+                        	</div>
+                        	<img src="../img/diplome-blog.png" class="img-responsive" alt="">
+						</a>
+					</div>
+					<div class="col-sm-4 portfolio-item">
+						<a href="#portfolioItem1" class="portfolio-link" data-toggle="modal">
+							<div class="caption">
+                            	<div class="caption-content">
+                                	<i class="fa fa-search-plus fa-3x"></i>
+                            	</div>
+                        	</div>
+                        	<img src="../img/game_controller.png" class="img-responsive" alt="">
+						</a>
+					</div>
+					<div class="col-sm-4 portfolio-item">
+						<a href="#portfolioItem1" class="portfolio-link" data-toggle="modal">
+							<div class="caption">
+                            	<div class="caption-content">
+                                	<i class="fa fa-search-plus fa-3x"></i>
+                            	</div>
+                        	</div>
+                        	<img src="../img/rect3388.png" class="img-responsive" alt="">
+						</a>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="CV">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 text-center">
+						<h2>Curriculum Vitae</h2>
+						<hr class="star-primary"/>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-4 col-lg-offset-2">
+                    	<p>Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional LESS stylesheets for easy customization.</p>
+                	</div>
+                	<div class="col-lg-4">
+                    	<p>Whether you're a student looking to showcase your work, a professional looking to attract clients, or a graphic artist looking to share your projects, this template is the perfect starting point!</p>
+                	</div>
+				</div>
+			</div>
+		</section>
 	</body>
 </html>
