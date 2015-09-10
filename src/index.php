1577ca6f319800cefcfcc1914e5fa4a693d7734a
Kévin <Kvin@192.168.1.36>
2015-09-10 16:39:45 +0200

    modification de l'index et du css lié

--- src/index.php
+++ src/index.php
@@ -95,5 +95,59 @@
 				</div>
 			</div>
 		</section>
+		<section id="contact">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 text-center">
+						<h2>Contactez moi !</h2>
+						<hr class="star-primary-c"/>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-6 col-lg-offset-3">
+						<form>
+							<div class="form-group">
+								<label for="contactMe-nom"> Nom</label>
+								<input type="text" class="form-control" id="contactMe-nom" placeholder="Nom">
+							</div>
+							<div class="form-group">
+								<label for="contactMe-prenom"> Prénom</label>
+								<input type="text" class="form-control" id="contactMe-prenom" placeholder="Prénom">
+							</div>
+							<div class="form-group">
+								<label for="contactMe-email"> Email</label>
+								<input type="email" class="form-control" id="contactMe-email" placeholder="Email">
+							</div>
+							<div class="form-group">
+								<label for="contactMe-num"> Numéro de téléphone</label>
+								<input type="tel" class="form-control" id="contactMe-num" placeholder="Numero">
+							</div>
+							<div class="form-group">
+								<label for="contactMe-num"> Message</label>
+								<textarea class="form-control" rows="5" placeholder="Entrez votre message ici ..."></textarea>
+							</div>
+							 <button type="submit" class="btn btn-default">Envoyer</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
+		<footer>
+			<div class="footer-above">
+				<div class="container text-center">
+					<div class="row">
+						<div class="col-lg-3 col-lg-offset-3">
+							<p>Kévin Letupe <br/>Etudiant à Polytech Lille<br/>Developpeur Java/Web</p>
+						</div>
+						<div class="col-lg-3">
+							<p>Me suivre sur internet :</p>
+							<a href="https://www.facebook.com/kletupe"><i class="fa fa-facebook-square fa-3x"></i></a>
+							<a href="https://fr.linkedin.com/pub/k%C3%A9vin-letupe/8a/924/240"><i class="fa fa-linkedin-square fa-3x"></i></a>
+							<a href="https://twitter.com/kevinn5914"><i class="fa fa-twitter-square fa-3x"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
 	</body>
 </html>
